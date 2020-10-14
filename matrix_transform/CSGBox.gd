0fc509997874359b08b9809aea76d7303947d9cf
Connor-GH <72793802+Connor-GH@users.noreply.github.com>
2020-10-14 04:57:45 -0500

    Add files via upload

---
+++ matrix_transform/CSGBox.gd
@@ -0,0 +1,19 @@
+extends CSGBox
+create_collider(Vector3.ZERO)
+		collision_layer = 0
+		collision_mask = 0
+	
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
