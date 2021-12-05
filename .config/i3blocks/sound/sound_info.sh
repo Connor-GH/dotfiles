c07f415293b9376b2fb0ebca4dd32a68c1810b02
Connor-GH <72793802+Connor-GH@users.noreply.github.com>
2021-12-05 20:09:56 +0000

    Add files via upload

---
+++ .config/i3blocks/sound/sound_info.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+VOLUME_MUTE="🔇"
+VOLUME_LOW="🔈"
+VOLUME_MID="🔉"
+VOLUME_HIGH="🔊"
+SOUND_LEVEL=$(pamixer --get-volume-human | cut -c 1-3)
+
+if [ "$SOUND_LEVEL" = "0" ]
+then
+    ICON="$VOLUME_MUTE"
+else
+    if [ "$SOUND_LEVEL" -lt 34 ]
+    then
+        ICON="$VOLUME_LOW"
+    elif [ "$SOUND_LEVEL" -lt 67 ]
+    then
+        ICON="$VOLUME_MID"
+    else
+        ICON="$VOLUME_HIGH"
+    fi
+fi
+
+echo "$ICON" "$SOUND_LEVEL"%
+
