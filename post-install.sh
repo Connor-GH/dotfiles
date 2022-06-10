9b73c033e231f3495057c6ddc994ff6ed4a9e1e7
Connor-GH <72793802+Connor-GH@users.noreply.github.com>
2022-06-10 07:23:32 +0000

    Update post-install.sh

--- post-install.sh
+++ post-install.sh
@@ -9,14 +9,16 @@
 
 cpubrand="amd" # amd or intel
 
-#archlinux. Will need AUR.
+#arch
 
-arch=`yay -S alacritty-git $cpubrand-ucode cava curl git discord dmenu \
-	feh ffmpeg firefox freetube gawk htop i3-gaps i3blocks \
-	lm_sensors lsb-release minecraft-launcher moc networkmanager
-	nfs-utils noto-fonts noto-fonts-emoji openrazer-meta pacman-contrib \
-	pavucontrol pciutils picom-ibhagwan-git screengrab ttf-hack ufw \
+arch=`pacman -S alacritty $cpubrand-ucode curl git dmenu \
+	feh ffmpeg firefox gawk htop i3-gaps i3blocks \
+	lm_sensors lsb-release networkmanager \
+	nfs-utils noto-fonts noto-fonts-emoji \
+	pavucontrol pciutils picom screengrab ttf-hack ufw \
 	unzip zip wine youtube-dl`
+# arch also needs openrazer, which is from the AUR,
+# but it's one package, so no need for an AUR helper
 
 #gentoo
 
@@ -26,27 +28,20 @@ gentoo=`emerge --ask alacritty $cpubrand-ucode curl git net-im/discord-bin dmenu
 	networkmanager nfs-utils noto noto-emoji pavucontrol pciutils \
 	screengrab eselect-repository hack ufw unzip zip wine youtube-dl`
 
-
-# gentoo needs cava compiled from github, no default repo
-# https://github.com/karlstav/cava
-
-# gentoo needs freetube compiled from github, no default repo
+# both distros need to compile this from github:
 # https://github.com/FreeTubeApp/FreeTube
 
-# gentoo needs picom ibhagwan fork compiled from github, no default repo
-# https://github.com/ibhagwan/picom
-
-# gentoo uses an ovelay for openrazer:
+# gentoo uses an overlay for openrazer:
 # `eselect repository enable vifino-overlay`
 # `emaint sync -r vifino-overlay`
 # `emerge --ask app-misc/openrazer`
 
 
-# gentoo uses an ovelay for openrazer:
+# gentoo uses an overlay for steam:
 # `eselect repository enable steam-overlay`
 # `emaint sync -r steam-overlay`
-# echo "*/*::steam-overlay" >> /etc/portage/package.accept_keywords (file)
-# `emerge --ask games-util/steam-launcher` (or steam-meta if that fails)
+# echo "*/*::steam-overlay" >> /etc/portage/package.accept_keywords # (file)
+# `emerge --ask games-util/steam-launcher` # (or steam-meta if that fails)
 
 
 # either uncomment out "$arch" or "$gentoo" depending on the system this 
